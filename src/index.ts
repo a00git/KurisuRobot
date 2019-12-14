bac2c7feb6c8b202722f9ae33f9b244e78d13d5d
a00git <a00klr@gmail.com>
2019-12-14 21:45:18 +0400

    adjust some things for Heroku

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import Telegraf from 'telegraf';
+import http from 'http';
 
 const bot = new Telegraf(process.env.BOT_TOKEN || '');
 bot.on('message', (ctx) => ctx.reply('Test~'));
 
 const main = async () => {
-  await bot.launch();
+  // await bot.launch();
+  http.createServer((req, res) => {
+    res.end('Test~');
+  }).listen();
 };
 
 main().catch(err => console.error(err));
