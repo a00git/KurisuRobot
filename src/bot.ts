5985abb1d8e2b520491fe443e1eb4637a1a808a1
a00git <a00klr@gmail.com>
2019-12-15 20:45:12 +0400

    try to get DB's health in inline mode

--- src/bot.ts
+++ src/bot.ts
@@ -1,4 +1,6 @@
 import Telegraf from 'telegraf';
+import fetch from 'node-fetch';
+import { JSDOM } from 'jsdom';
 
 const bot = new Telegraf(process.env.BOT_TOKEN || '');
 
@@ -8,6 +10,25 @@ bot.command('send', ctx => {
 
 bot.command('receive', ctx => {
   ctx.reply('This command will generate random messages from the future');
+});
+
+bot.on('inline_query', async ({ inlineQuery, answerInlineQuery }) => {
+  if (inlineQuery?.query !== 'db') {
+    return;
+  }
+
+  const response = await fetch('https://dreamingrobot.herokuapp.com/');
+  const html = await response.text();
+  const dom = new JSDOM(html);
+  const hps = dom.window.document.querySelectorAll('.heart').length;
+  return answerInlineQuery([{
+    id: '1',
+    type: 'article',
+    title: `DB's health`,
+    input_message_content: {
+      message_text: `DB is at ${hps} hearts out of 5`,
+    },
+  }]);
 });
 
 export default bot;
