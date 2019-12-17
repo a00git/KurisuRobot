4d0f4779c7967827244425c0b1d5de2a34662686
a00git <a00klr@gmail.com>
2019-12-17 21:24:05 +0400

    move url to variable

--- src/dreambot.ts
+++ src/dreambot.ts
@@ -1,15 +1,18 @@
 import fetch from 'node-fetch';
 import { JSDOM } from 'jsdom';
 
-export const parseHtml = (html: string): HTMLImageElement[] => {
+const dreambotPage = 'https://dreamingrobot.herokuapp.com/';
+
+const parseHtml = (html: string): HTMLImageElement[] => {
   const dom = new JSDOM(html);
   const htmlCollection = dom.window.document.querySelectorAll('a')[1].children;
   return Array.from(htmlCollection) as HTMLImageElement[];
 };
 
 export const getDBsHealth = async (): Promise<number> => {
-  const response = await fetch('https://dreamingrobot.herokuapp.com/');
+  const response = await fetch(dreambotPage);
   const html = await response.text();
+
   return parseHtml(html).reduce((hp, img) => {
     switch (img.alt) {
       case 'Full heart': return hp + 1;
