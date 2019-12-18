daf1d8bdd3af89092f98dff4576ff21367c7ad67
a00git <a00klr@gmail.com>
2019-12-18 11:29:48 +0400

    move bot helpers to `utils`

--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,21 @@
+import { ContextMessageUpdate } from 'telegraf';
+
 export const randomElementFrom = <T>(arr: T[]): T => arr[Math.floor(Math.random() * arr.length)];
+
+export type ChainableFunc<T> = (arg: T) => T;
+export const compose = <R>(...funcs: ChainableFunc<R>[]) => (target: R): R => funcs.reduce((t, f) => f(t), target);
+
+export const extractTextFromUpdate = (ctx: ContextMessageUpdate): string => {
+  switch (ctx.updateType) {
+    case 'message':
+      return ctx.message?.text ?? ctx.message?.sticker?.emoji ?? 'can\'t display message';
+    case 'edited_message':
+      return ctx.editedMessage?.text || 'can\'t display edit';
+    case 'inline_query':
+      return ctx.inlineQuery?.query || 'can\'t display query';
+    case 'chosen_inline_result':
+      return ctx.chosenInlineResult?.query || 'can\'t display chosen query';
+    default:
+      return 'unsupported type';
+  }
+};
